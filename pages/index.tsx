7528a725bbfd84d89fbf76ddc9106e5a30a28a8a
Joey Burke <joeybrk372@gmail.com>
2023-02-24 15:51:55 -0800

    Add initial tool kit section

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,27 @@ import stravaImg from '../public/assets/work/logos/strava.png'
 import googleMapsImg from '../public/assets/work/logos/googlemaps.png'
 import justTheTipImg from '../public/assets/work/just-the-tip.jpg'
 import solvedInMotionImg from '../public/assets/work/solved-in-motion.jpeg'
+import figmaImg from '../public/assets/work/logos/figma.png'
+import xdImg from '../public/assets/work/logos/xd.png'
+import vsImg from '../public/assets/work/logos/vs.png'
+import githubImg from '../public/assets/work/logos/github.png'
+import idImg from '../public/assets/work/logos/id.png'
+import aiImg from '../public/assets/work/logos/ai.png'
+import psImg from '../public/assets/work/logos/ps.png'
+import gsuiteImg from '../public/assets/work/logos/gsuite.png'
+import msImg from '../public/assets/work/logos/ms.png'
+
+const toolIcons = [
+  figmaImg,
+  xdImg,
+  vsImg,
+  githubImg,
+  idImg,
+  aiImg,
+  psImg,
+  gsuiteImg,
+  msImg,
+]
 
 
 const Home: NextPage = () => {
@@ -46,7 +67,7 @@ const Home: NextPage = () => {
         </div>
       </div>
 
-      <section css={sectionCss}>
+      <section css={[sectionCss, css`margin: 90px auto;`]}>
         <h2>What I've Done</h2>
         <p>My work is a collection of self-led and created projects. The case studies, optimizations and design work represent my willingness to learn and passion for enabling users to reach their goals.</p>
         <div css={css`display: flex; margin-top: 90px;`}>
@@ -74,6 +95,29 @@ const Home: NextPage = () => {
         </div>
       </section>
 
+      <div css={css`background: ${COLORS.primary}; padding: 50px 0 100px 0;`}>
+      <section css={[sectionCss, css`color: #fff; `]}>
+        <div css={css`height: 12px; width: 175px; background: ${COLORS.secondary}; margin-bottom: 50px;`} />
+        <h2>Tool Kit</h2>
+        <p>I taught myself Figma, Adobe XD, HTML and CSS with the help of online learning platforms. I have years of professional experience in Adobe programs such as Photoshop, Illustrator and InDesign. I have worked in Google Suite and Microsoft Office team environments. I am eager to learn new tools and deepen my knowledge.</p>
+        <ul css={css`list-style-type: none; padding: none; display: flex; gap: 30px; margin: 90px 0; justify-content: center;`}>{toolIcons.map((toolIcon) => <li><Image src={toolIcon} height={75} width={75} /></li>)}</ul>
+
+        <div css={css`display: flex;`}>
+          <h3 css={css`flex: 0 0 150px; height: 100%; padding-top: 40px;`}>Certifications</h3>
+          <div css={css`display: flex; gap: 35px; flex: 1 1 100%;`}>
+            <div css={css`display: flex; flex-direction: column; justify-content: flex-end; background: #fff; height: 200px; border-radius: 35px; padding: 35px;`}>
+              <img css={css`margin-bottom: 15px;`} src="/assets/work/logos/codecademy.png" width={157} />
+              <p css={css`font-weight: bold; font-size: 20px; color: #000; margin: 2px 0;`}>Website Building Skill Path</p>
+              <p css={css`font-weight: bold; font-size: 20px; color: #676767; margin: 2px 0;`}>HTML, CSS and GitHub</p>
+            </div>
+            <div css={css`display: flex; flex-direction: column; justify-content: flex-end; background: #fff; height: 200px; border-radius: 35px; padding: 35px;`}>
+              <img css={css`margin-bottom: 15px;`} src="/assets/work/logos/coursera.png" width={232} />
+              <p css={css`font-weight: bold; font-size: 20px; margin: 2px 0; color: #000; margin-top: 24px;`}>Google UX Design Professional Certificate</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      </div>
     </div>
   )
 }
