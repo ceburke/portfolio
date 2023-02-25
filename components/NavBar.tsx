067895096bba19ceecfe31f35fd5fc8ef8c602f8
Joey Burke <joeybrk372@gmail.com>
2023-02-24 17:05:07 -0800

    Clean up homepage styling

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -10,6 +10,7 @@ const baseNavItemCss = css`
     justify-content: center;
     width: 100px;
     height: 100%;
+    margin-right: 100px;
     border-top: 12px solid transparent;
     cursor: pointer;
     transition: all 300ms ease;
@@ -17,6 +18,10 @@ const baseNavItemCss = css`
 
     &:hover {
         opacity: 0.8;
+    }
+
+    &:last-of-type {
+        margin-right: 0;
     }
 `
 
@@ -38,12 +43,22 @@ export default function NavBar(): JSX.Element {
     return (
         <header css={css`display: flex; align-items: center; height: 100px;`}>
             <div css={[sectionCss, css`display: flex; align-items: center; justify-content: space-between; height: 100%;`]}>
-            <span css={css`color: ${COLORS.secondary}; font-weight: bold; font-size: 40px; line-height: 48px; text-transform: lowercase; margin-top: 12px;`}>Claire Burke</span>
-            <nav css={css`height: 100%; width: 800px; text-align: right; display: flex; align-items: center; justify-content: space-evenly;`}>
+            <span css={css`display: inline-block; color: ${COLORS.secondary}; font-weight: bold; font-size: 40px; line-height: 48px; text-transform: lowercase; margin-top: 12px;`}>Claire Burke</span>
+            <nav css={css`height: 100%; width: 800px; text-align: right; display: flex; align-items: center; justify-content: flex-end;`}>
                 <a css={[baseNavItemCss, pageNavItemCss, router.pathname === '/' && activeItemCss]} href="/">work</a>
                 <a css={[baseNavItemCss, pageNavItemCss, router.pathname === '/about' && activeItemCss]} href="/about">about</a>
-                <a css={[baseNavItemCss, navItemCss]} href="/resume.pdf" about="_blank"><Image src="/assets/paperclip.png" height={25} width={25} />resume</a>
-                <a css={[baseNavItemCss, navItemCss]} href="mailto:claireburke1130@gmail.com" about="_blank"><Image src="/assets/mail.png" height={25} width={25} />email</a>
+                <a css={[baseNavItemCss, navItemCss]} href="/resume.pdf" about="_blank">
+                    <div css={css`flex: 0 0 auto; position: relative; margin-right: 10px; height: 17px; width: 17px`}>
+                        <Image src="/assets/paperclip.png" layout="fill" />
+                    </div>
+                    resume
+                </a>
+                <a css={[baseNavItemCss, navItemCss]} href="mailto:claireburke1130@gmail.com" about="_blank">
+                    <div css={css`flex: 0 0 auto; position: relative; margin-right: 10px; height: 16px; width: 22px`}>
+                        <Image src="/assets/mail.png" layout="fill" />
+                    </div>
+                    email
+                </a>
             </nav>
             </div>
         </header>
--- pages/index.tsx
+++ pages/index.tsx
@@ -52,12 +52,13 @@ const Home: NextPage = () => {
         <NavBar />
         <div css={css`z-index: -1; position: absolute; top: 0; left: 0; right: 0; bottom: 0; background: ${COLORS.primary}`}></div>
         <section css={sectionCss}>
-        <h1>
+        <h1 css={css`font-size: 36px; font-weight: 400; line-height: 42px; margin-top: 100px;`}>
           Hi there, I’m Claire,<br />
-          a <span css={css`color: ${COLORS.secondary}`}>product designer</span> with an eye for<br />
-          <span css={css`color: ${COLORS.secondary}`}>creating community</span> and a passion for <span css={css`color: ${COLORS.secondary}`}>outdoor recreation</span>
+          a <span css={css`font-weight: bold; color: ${COLORS.secondary}`}>product designer</span> with an eye for<br />
+          <span css={css`font-weight: bold; color: ${COLORS.secondary}`}>creating community</span> and a <br />
+          passion for <span css={css`font-weight: bold; color: ${COLORS.secondary}`}>outdoor recreation</span>
         </h1>
-        <p>
+        <p css={css`font-size: 20px;`}>
           I am a designer by way of scientific communication, environmental consulting and printmaking.<br/> 
           My experience gives me a fresh approach to UX/UI and an insatiable curiosity for learning. <Link href="/about" >Read More</Link>
         </p>
@@ -68,10 +69,10 @@ const Home: NextPage = () => {
       </div>
 
       <section css={[sectionCss, css`margin: 90px auto;`]}>
-        <h2>What I've Done</h2>
-        <p>My work is a collection of self-led and created projects. The case studies, optimizations and design work represent my willingness to learn and passion for enabling users to reach their goals.</p>
+        <h2 css={css`font-weight: bold; font-size: 40px;`}>What I've Done</h2>
+        <p css={css`font-size: 20px;`}>My work is a collection of self-led and created projects. The case studies, optimizations and design work represent my willingness to learn and passion for enabling users to reach their goals.</p>
         <div css={css`display: flex; margin-top: 90px;`}>
-          <h3 css={css`flex: 0 0 150px; height: 100%; padding-top: 40px;`}>Case Studies</h3>
+          <h3 css={css`flex: 0 0 200px; height: 100%; padding-top: 40px; font-size: 24px;`}>Case Studies</h3>
           <div css={css`width: 100%;`}>
             <Card size="large" title="Type 1 Meet" subtitle="Community and resource sharing platform for people with chronic disease" image={type1MeetImg}><p>bacon ipsum</p></Card>
             <Card size="large"  css={css`margin-top: 35px`} title="Southwest Print Guild" subtitle="Expanding professional networks and community building in a rural region" image={swpgImg} />
@@ -79,7 +80,7 @@ const Home: NextPage = () => {
         </div>
 
         <div css={css`display: flex; margin-top: 90px;`}>
-          <h3 css={css`flex: 0 0 150px; height: 100%; padding-top: 40px;`}>Optimization</h3>
+          <h3 css={css`flex: 0 0 200px; height: 100%; padding-top: 40px; font-size: 24px;`}>Optimization</h3>
           <div css={css`display: flex; gap: 35px; flex: 1 1 100%;`}>
             <Card size="medium" title="Strava" subtitle="Integrating trail conditions reporting with activities and heat maps" image={stravaImg} />
             <Card size="medium" title="Google Maps" subtitle="User flow for planned trips with custom alarms and live conditions" image={googleMapsImg} />
@@ -87,7 +88,7 @@ const Home: NextPage = () => {
         </div>
 
         <div css={css`display: flex; margin-top: 90px; width: 100%;`}>
-          <h3 css={css`flex: 0 0 150px; height: 100%; padding-top: 40px;`}>Design</h3>
+          <h3 css={css`flex: 0 0 200px; height: 100%; padding-top: 40px; font-size: 24px;`}>Design</h3>
           <div css={css`display: flex; gap: 35px; width: 100%;`}>
             <Card size="medium" title="Book Covers" subtitle="A series of imaginary book covers inspired by a love of reading and design" image={justTheTipImg} imageType="cover" />
             <Card size="medium" title="Solved in motion" subtitle="Immersive art installation featuring a local trail system" image={solvedInMotionImg} imageType="cover"  />
@@ -98,12 +99,12 @@ const Home: NextPage = () => {
       <div css={css`background: ${COLORS.primary}; padding: 50px 0 100px 0;`}>
       <section css={[sectionCss, css`color: #fff; `]}>
         <div css={css`height: 12px; width: 175px; background: ${COLORS.secondary}; margin-bottom: 50px;`} />
-        <h2>Tool Kit</h2>
-        <p>I taught myself Figma, Adobe XD, HTML and CSS with the help of online learning platforms. I have years of professional experience in Adobe programs such as Photoshop, Illustrator and InDesign. I have worked in Google Suite and Microsoft Office team environments. I am eager to learn new tools and deepen my knowledge.</p>
+        <h2 css={css`font-weight: bold; font-size: 40px;`}>Tool Kit</h2>
+        <p css={css`font-size: 20px;`}>I taught myself Figma, Adobe XD, HTML and CSS with the help of online learning platforms. I have years of professional experience in Adobe programs such as Photoshop, Illustrator and InDesign. I have worked in Google Suite and Microsoft Office team environments. I am eager to learn new tools and deepen my knowledge.</p>
         <ul css={css`list-style-type: none; padding: none; display: flex; gap: 30px; margin: 90px 0; justify-content: center;`}>{toolIcons.map((toolIcon) => <li><Image src={toolIcon} height={75} width={75} /></li>)}</ul>
 
         <div css={css`display: flex;`}>
-          <h3 css={css`flex: 0 0 150px; height: 100%; padding-top: 40px;`}>Certifications</h3>
+          <h3 css={css`flex: 0 0 200px; height: 100%; padding-top: 40px; font-size: 24px;`}>Certifications</h3>
           <div css={css`display: flex; gap: 35px; flex: 1 1 100%;`}>
             <div css={css`display: flex; flex-direction: column; justify-content: flex-end; background: #fff; height: 200px; border-radius: 35px; padding: 35px;`}>
               <img css={css`margin-bottom: 15px;`} src="/assets/work/logos/codecademy.png" width={157} />
