ef3a1803d10017d8d7f53276b0ba3910bb2bd617
Joey Burke <joeybrk372@gmail.com>
2023-03-02 07:46:10 -0800

    Revert to css variable for hero parallax

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import { css } from "@emotion/react";
-import { throttle } from "lodash";
-import type { NextPage } from "next";
+import {css} from "@emotion/react";
+import {throttle} from "lodash";
+import type {NextPage} from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { useEffect } from "react";
+import {useEffect} from "react";
 import BookCoversCard from "../components/cards/BookCoversCard";
 import GoogleMapsCard from "../components/cards/GoogleMapsCard";
 import SolvedInMotionCard from "../components/cards/SolveInMotionCard";
@@ -12,8 +12,7 @@ import SWPGCard from "../components/cards/SWPGCard";
 import Type1MeetCard from "../components/cards/Type1MeetCard";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
-import ParallaxHeroImage from "../components/ParallaxHeroImage";
-import { BASE_PATH, BREAKPOINT_SMALL, COLORS } from "../constants";
+import {BASE_PATH, BREAKPOINT_SMALL, COLORS} from "../constants";
 import aiImg from "../public/assets/work/logos/ai.png";
 import figmaImg from "../public/assets/work/logos/figma.png";
 import githubImg from "../public/assets/work/logos/github.png";
@@ -23,11 +22,25 @@ import msImg from "../public/assets/work/logos/ms.png";
 import psImg from "../public/assets/work/logos/ps.png";
 import vsImg from "../public/assets/work/logos/vs.png";
 import xdImg from "../public/assets/work/logos/xd.png";
-import { sectionCss } from "../styles/styles";
+import {sectionCss} from "../styles/styles";
 
 const toolIcons = [figmaImg, xdImg, vsImg, githubImg, idImg, aiImg, psImg, gsuiteImg, msImg];
 
 const Home: NextPage = () => {
+  // adjust CSS scroll variable on window scroll, used for hero "parallax"
+  useEffect(() => {
+    const onScroll = throttle(() => {
+      document.body.style.setProperty(
+        "--scroll",
+        `${window.pageYOffset / (document.body.offsetHeight - window.innerHeight)}`
+      );
+    }, 1000 / 30);
+    window.addEventListener("scroll", onScroll, false);
+    return () => {
+      window.removeEventListener("scroll", onScroll, false);
+    };
+  }, []);
+
   return (
     <div>
       <Head>
@@ -220,7 +233,29 @@ const Home: NextPage = () => {
             </p>
           </section>
 
-          <ParallaxHeroImage />
+          {/* hero image */}
+          <div
+            css={css`
+              position: relative;
+              width: 1200px;
+              max-width: 95vw;
+              height: 450px;
+              top: 100px;
+              margin: 0 auto;
+              background: url(/assets/work/hero.jpg);
+              background-size: cover;
+              background-position-y: calc(-200px - 2000px * var(--scroll));
+              background-repeat: no-repeat;
+
+              @media (max-width: ${BREAKPOINT_SMALL}px) {
+                top: 0;
+                width: 100vw;
+                max-width: none;
+                height: 250px;
+                background-position-y: calc(-20px - 1000px * var(--scroll));
+              }
+            `}
+          />
         </div>
       </div>
 
