0a2d4611a75e96801709ac941f840e380596b8c8
Joey Burke <joeybrk372@gmail.com>
2024-12-01 17:02:40 -0500

    Refine styles of PostPreview

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -4,19 +4,21 @@ import Link from 'next/link';
 
 export default function PostPreview({post}: {post: PostMetadata}) {
   return (
-    <div className="w-full border-[1px]">
+    <div className="w-full">
       <Link className="block overflow-hidden h-[330px]" href={`/posts/${post.id}`}>
         <Image
-          className="object-cover w-full h-full"
+          className="object-cover w-full h-full transition-all duration-300 ease-in-out scale-100 hover:scale-110"
           src={post.previewImg}
           alt={post.description}
         />
       </Link>
-      <h2>
-        <Link href={`/posts/${post.id}`}>{post.title}</Link>
-      </h2>
-      <p>{post.subtitle}</p>
-      <p>{post.description}</p>
+      <div className="flex flex-col gap-2 px-10 mt-6">
+        <h2 className="text-2xl font-bold">
+          <Link href={`/posts/${post.id}`}>{post.title}</Link>
+        </h2>
+        <h3 className="text-xl font-bold">{post.subtitle}</h3>
+        <p>{post.description}</p>
+      </div>
     </div>
   );
 }
