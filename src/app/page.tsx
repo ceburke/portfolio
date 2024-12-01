d622eaf9047f6806ff751c2b0b29c24ad8a32b46
Joey Burke <joeybrk372@gmail.com>
2024-11-30 21:18:47 -0500

    Scaffold navbar with light + dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="dark dark:bg-black dark:text-white grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="dark dark:bg-black dark:text-white items-center justify-items-center min-h-screen">
+      <Navbar />
       <Link href="/blog/information-architecture-and-responsive-design">
         Information architecture and responsive design
       </Link>
+      <Footer />
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,29 @@
+import {MAX_CONTENT_WIDTH} from '@/constants';
 import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">Work</Link>
-          <Link href="/about">About</Link>
-        </li>
-      </ul>
+    <nav className="sticky h-14 p-2 w-full text-center bg-white dark:bg-black dark:text-white">
+      <div
+        className="flex justify-between items-center mx-auto"
+        style={{maxWidth: `${MAX_CONTENT_WIDTH}px`}}
+      >
+        <Link href="/">CB</Link>
+        <ul className="flex items-center gap-4">
+          <li>
+            <Link href="/">Work</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/about">Resume</Link>
+          </li>
+          <li>
+            <Link href="/about">Contact</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
