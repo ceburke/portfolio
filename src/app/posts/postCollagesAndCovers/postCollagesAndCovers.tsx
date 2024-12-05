67d4c1c46237d1198a28db0e9048fb331040c33c
Curb <claireburke1130@gmail.com>
2024-12-04 23:13:21 -0700

    I'm oh so tired

--- src/app/posts/postCollagesAndCovers/postCollagesAndCovers.tsx
+++ src/app/posts/postCollagesAndCovers/postCollagesAndCovers.tsx
@@ -1,9 +1,28 @@
-import { Heading1 } from "../postComponents";
+import { ToolData, toolsData } from "@/components/tools/toolsData";
+import {
+  Double,
+  ExtendedPostTitle,
+  Heading2,
+  Heading3,
+  PostImage,
+  Triple,
+} from "../postComponents";
+import collage1 from "./collage1.jpg";
 
 export default function PostCollagesAndCovers() {
   return (
     <>
-      <Heading1>Covers and collages</Heading1>
+      <ExtendedPostTitle
+        title="Covers and collages"
+        description="Collage has always been one of my favorite creative mediums, whether working digitally or with physical materials. Collage offers endless opportunities for exploration, making it a medium where imagination can truly run wild.
+
+Thanks to a fortuitous stack of National Geographic magazines I stumbled upon in an alley, I was able to dive into a series of collages. This series evolved into a collection of book cover designs that playfully experiment with storytelling and visual balance."
+      />
+
+      <PostImage
+        src={collage1}
+        description="A selection of collages and book covers created from found images."
+      />
     </>
   );
 }
