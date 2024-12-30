4ec0ade70a03c3377f7be87a5f1366fc0dfd56bc
Curb <claireburke1130@gmail.com>
2024-12-30 15:16:00 -0700

    Add Google Analytics tag to head in layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,24 @@
-import type {Metadata} from 'next';
-import {Emblema_One, Inter} from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Emblema_One, Inter } from "next/font/google";
+import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-inter',
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
 });
 
 const emblemaOne = Emblema_One({
-  weight: '400',
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-emblema-one',
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-emblema-one",
 });
 
 export const metadata: Metadata = {
-  title: 'Claire Burke',
-  description: 'A designer and developer creating for positive impact.',
+  title: "Claire Burke",
+  description: "A designer and developer creating for positive impact.",
 };
 
 export default function RootLayout({
@@ -27,6 +28,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-14B5S8FHZJ"></Script>
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-14B5S8FHZJ');
+        `}
+        </Script>
+      </head>
       <body className={`${inter.variable} ${emblemaOne.variable} antialiased`}>{children}</body>
     </html>
   );
