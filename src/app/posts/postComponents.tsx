3965884e8e285aed79227f501ab6fcedc5394255
Curb <claireburke1130@gmail.com>
2025-03-13 23:23:52 -0600

    Revert all changes on Extended

--- src/app/posts/postComponents.tsx
+++ src/app/posts/postComponents.tsx
@@ -4,14 +4,8 @@ import ToolsList from "@/components/tools/ToolsList";
 import ExportedImage from "next-image-export-optimizer";
 import { StaticImageData } from "next/image";
 
-export function Heading1({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}): React.ReactNode {
-  return <h1 className={`text-5xl font-bold mb-1 ${className}`}>{children}</h1>;
+export function Heading1({ children }: { children: React.ReactNode }): React.ReactNode {
+  return <h1 className="text-4xl font-bold mb-1">{children}</h1>;
 }
 
 export function Heading2({ children }: { children: React.ReactNode }): React.ReactNode {
@@ -22,17 +16,9 @@ export function Heading3({ children }: { children: React.ReactNode }): React.Rea
   return <h3 className="text-xl font-bold mb-1">{children}</h3>;
 }
 
-export function Double({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}): React.ReactNode {
+export function Double({ children }: { children: React.ReactNode }): React.ReactNode {
   return (
-    <section
-      className={`grid grid-cols-1 md:grid-cols-2 gap-2 lg:gap-5 items-center justify-center ${className}`}
-    >
+    <section className="grid grid-cols-1 md:grid-cols-2 gap-2 lg:gap-5 items-center justify-center">
       {children}
     </section>
   );
@@ -85,45 +71,36 @@ export function ExtendedPostTitle({
 }): React.ReactNode {
   return (
     <div className="flex flex-col justify-center gap-5 pb-10 my-10">
-      <div className="flex flex-col lg:flex-row justify-between items-start gap-5 pb-3">
-        <div className="w-full lg:w-2/5">
-          <Heading1 className="mb-5">{title}</Heading1>
-          {toolsData && (
-            <>
-              <Heading2>Primary Tools</Heading2>
-              <ToolsList toolsData={toolsData} />
-            </>
-          )}
-        </div>
-
-        <div className="w-full lg:w-3/5 lg:ml-5">
-          {!!description && <p className="mb-5">{description}</p>}
-          <Double>
-            {roles && (
-              <section className="h-full">
-                <>
-                  <Heading2>Responsibilities</Heading2>
-                  <ul className="mt-5 list-disc pl-6">
-                    {roles.map((role, i) => (
-                      <li key={i}>{role}</li>
-                    ))}
-                  </ul>
-                </>
-              </section>
-            )}
-            {results && (
-              <section className="h-full">
-                <Heading2>Results</Heading2>
-                <ul className="mt-5 list-disc pl-6">
-                  {results.map((result, i) => (
-                    <li key={i}>{result}</li>
-                  ))}
-                </ul>
-              </section>
-            )}
-          </Double>
-        </div>
-      </div>
+      <Heading1>{title}</Heading1>
+      {!!description && <p>{description}</p>}
+      <Double>
+        {roles && (
+          <section className="h-full">
+            <Heading2>Responsibilities</Heading2>
+            <ul className="mt-5 list-disc pl-6">
+              {roles.map((role, i) => (
+                <li key={i}>{role}</li>
+              ))}
+            </ul>
+          </section>
+        )}
+        {results && (
+          <section className="h-full">
+            <Heading2>Results</Heading2>
+            <ul className="mt-5 list-disc pl-6">
+              {results.map((result, i) => (
+                <li key={i}>{result}</li>
+              ))}
+            </ul>
+          </section>
+        )}
+      </Double>
+      {toolsData && (
+        <>
+          <Heading2>Primary Tools</Heading2>
+          <ToolsList toolsData={toolsData} />
+        </>
+      )}
     </div>
   );
 }
